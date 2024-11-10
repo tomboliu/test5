859cc79a745fe7815c25f2db67ab699793197c3a
Tom Liu <tomliu6116@hotmail.com>
2024-11-11 01:08:43 +0800

    修改程式，輸出本地時間 以確認 GitHub 執行 Action 時間與本地時間差
    使用 Poetry 管理 Package
    安裝了 pytz 套件

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+from datetime import datetime
+import pytz
 
-print("Hello World")
+tz = pytz.timezone('Asia/Taipei')
+
+local_time = datetime.now(tz)
+
+print(f"Hello World, Now is {local_time.strftime('%Y-%m-%d %H:%M:%S')} in Asia/Taipei")
